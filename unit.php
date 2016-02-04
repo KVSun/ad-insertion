dc7742f7fc95b81c635b5f5bcbccff441fc1dd53
kvsun-editor <editor@kvsun.com>
2016-02-03 16:28:51 -0800

    Require the autoloader

--- unit.php
+++ unit.php
@@ -3,6 +3,7 @@ if (PHP_SAPI !== 'cli') {
 	http_response_code(403);
 	exit();
 }
+require('./autoloader.php');
 echo get_include_path() . PHP_EOL;
 assert(true, 'This assert should pass.');
 assert(false, 'This assert should fail');
