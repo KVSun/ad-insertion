2e7cbfd87e81d5e4fda600908395d38eaf9137ab
kvsun-editor <editor@kvsun.com>
2016-02-03 16:13:08 -0800

    Add 2 asserts
    One should pass and the other should fail

--- unit.php
+++ unit.php
@@ -4,6 +4,8 @@ if (PHP_SAPI !== 'CLI') {
 	exit();
 }
 $timer = \shgysk8zer0\Core\Timer::load('Test');
+assert(true, 'This assert should pass.');
+assert(false, 'This assert should fail');
 
 echo "Completed in $timer\n";
 exit(0);
