68e177c2daa083a0826e23ec87ace4e38c04ae35
kvsun-editor <editor@kvsun.com>
2016-02-10 14:15:02 -0800

    Add README as modal dialog to footer
    Using Parsedown and `importHTML`

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,13 @@
 <?php
+$parsedown = new \Parsedown\Parsedown();
 $footer = $dom->body->append('footer');
+$readme = $footer->append('dialog', null, ['id' => 'README-dialog']);
+$readme->append('button', 'x', [
+	'type' => 'button',
+	'data-close-modal' => "#{$readme->id}"
+]);
+$readme->importHTML($parsedown->text(file_get_contents('README.md')));
 $footer->append('button', 'Contact', ['data-show-modal' => '#contact-dialog']);
+$footer->append('button', 'Documentation', ['data-show-modal' => "#{$readme->id}"]);
+unset($parsedown, $readme);
 require_once './components/contact-dialog.php';
