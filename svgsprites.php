38037df04575cfb092918c20bf6ccb3b882f24c4
Chris Zuber <shgysk8zer0@gmail.com>
2016-02-13 19:49:30 -0800

    Update icons.json to be an object
    It now has keys, so the names of the resulting icons can be customized.
    Requires parsing the JSON as an associative array.

--- svgsprites.php
+++ svgsprites.php
@@ -4,6 +4,6 @@ putenv('AUTOLOAD_DIR=classes');
 putenv('AUTOLOAD_FUNC=spl_autoload');
 putenv('AUTOLOAD_EXTS=.php');
 require_once './autoloader.php';
-$icons = json_decode(file_get_contents('images/icons.json'));
+$icons = json_decode(file_get_contents('images/icons.json'), true);
 $sprites = new \shgysk8zer0\DOM\SVGSprite($icons);
 $sprites->save('images/icons.svg');
