7478d331e05e06c4fc81e48bb504c1de76f65182
Chris Zuber <shgysk8zer0@gmail.com>
2016-02-20 11:16:35 -0800

    <head> is now constructed using \DOMDocumentFragment
    Also improves loading and construction of README-dialog.

--- components/head.php
+++ components/head.php
@@ -1,34 +1,36 @@
 <?php
 $dom = \shgysk8zer0\DOM\HTML::getInstance();
-$head = $dom->createDocumentFragment();
-$dom->head->append('title', 'Ad Insertion');
-$dom->head->append('link', null, [
+$frag = $dom->createDocumentFragment();
+
+$frag('title', 'Ad Insertion');
+$frag('link', null, [
 	'rel' => 'icon',
 	'href' => 'sun.svg',
 	'type' => 'image/svg+xml',
 	'sizes' => 'any'
 ]);
-$dom->head->append('link', null, [
+$frag('link', null, [
 	'rel' => 'stylesheet',
 	'href' => 'stylesheets/styles/styles.css',
 	'media' => 'all'
 ]);
-$dom->head->append('link', null, [
+$frag('link', null, [
 	'rel' => 'prefetch',
 	'href' => 'images/icons.svg',
 	'type' => 'image/svg+xml'
 ]);
-$dom->head->append('script', null, [
+$frag('script', null, [
 	'type' => 'application/javascript',
 	'src' => 'scripts/std-js/prototypes.es6',
 	'defer' => true
 ]);
-$dom->head->append('script', null, [
+$frag('script', null, [
 	'type' => 'application/javascript',
 	'src' => 'scripts/custom.js',
 	'defer' => true
 ]);
-$dom->head->append('meta', null, [
+$frag('meta', null, [
 	'name' => 'description',
 	'content' => 'An ad insertion form for the Kern Valley Sun'
 ]);
+return $frag;
--- index.php
+++ index.php
@@ -1,30 +1,23 @@
 <?php
 require_once './functions.php';
-$console = \shgysk8zer0\Core\Console::getInstance()->asErrorHandler()->asExceptionHandler();
-$headers = new \shgysk8zer0\Core\Headers();
+\shgysk8zer0\Core\Console::getInstance()->asErrorHandler()->asExceptionHandler();
+\shgysk8zer0\DOM\HTMLElement::$import_path = realpath(getenv('COMPONENTS_DIR'));
+$headers = \shgysk8zer0\Core\Headers::getInstance();
 if (in_array('text/html', explode(',', $headers->accept))) {
-	\shgysk8zer0\DOM\HTMLElement::$import_path = realpath(getenv('COMPONENTS_DIR'));
 	$dom = \shgysk8zer0\DOM\HTML::getInstance();
 	$dom->body->class = 'flex column';
-	$body = $dom->body;
-	\KVSun\load('head');
-	$body('header', 'main', 'forms/ad-insertion', 'footer');
+	call_user_func($dom->head, 'head');
+	call_user_func($dom->body, 'header', 'main', 'forms/ad-insertion', 'footer');
 	exit($dom);
 } elseif ($headers->accept === 'application/json' and !empty($_REQUEST)) {
 	$resp = \shgysk8zer0\Core\JSON_Response::load();
 	if (array_key_exists('load', $_REQUEST)) {
 		switch($_REQUEST['load']) {
 			case 'readme':
-				$parsedown = new \Parsedown\Parsedown();
 
 				$dom = \shgysk8zer0\DOM\HTML::getInstance();
-				$readme = $dom->body->append('dialog', null, ['id' => 'README-dialog']);
-				$readme->append('button', null, [
-					'type' => 'button',
-					'data-delete' => "#{$readme->id}"
-				]);
-				$readme->append('br');
-				$readme->importHTML($parsedown->text(file_get_contents('README.md')));
+				call_user_func($dom->body, 'readme');
+				$readme = $dom->getElementsByTagName('dialog')->item(0);
 				$resp->append('body', $readme);
 				$resp->showModal("#{$readme->id}");
 				break;
