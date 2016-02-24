19449376f560b21449103de9480ba1dbb877cff4
kvsun-editor <editor@kvsun.com>
2016-02-24 09:47:25 -0800

    Create ad-insertion form in a dialog
    And load it via `fetch`.
    
    Do not load it in main, and add a link in `<footer>` and a request
    handler in `index.php`.

--- components/main.php
+++ components/main.php
@@ -1,4 +1,2 @@
 <?php
-$main = \shgysk8zer0\DOM\HTML::getInstance()->createElement('main');
-$main('forms/ad-insertion');
-return $main;
+return \shgysk8zer0\DOM\HTML::getInstance()->createElement('main');
