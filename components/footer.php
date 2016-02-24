c4277de8a593a5d4079f1f032d1c4707dda575fa
Chris Zuber <shgysk8zer0@gmail.com>
2016-02-23 19:39:19 -0800

    Use `\shgysk8zer0\DOM\SVG::useIcon` for icons
    Simpler and cleaner code...
    
    Also, set width and height as an array and use that for all SVG
    dimensions.

--- components/footer.php
+++ components/footer.php
@@ -1,26 +1,15 @@
 <?php
+$size = ['height' => 50, 'width' => 50];
 $footer = \shgysk8zer0\DOM\HTML::getInstance()->createElement('footer');
 $footer->append('a', null, [
 	'title' => 'View README/documentation',
 	'href' => './?load=readme',
 	'role' => 'button'
-	// 'data-show-modal' => "#{$readme->id}"
-])->append('svg', null, [
-	'height' => 50,
-	'width' => 50
-])->append('use', null, [
-	'xlink:href' => 'images/icons.svg#book'
-]);
+])->import(\shgysk8zer0\DOM\SVG::useIcon('book', $size));
 $footer->append('button', null, [
 	'title' => 'Contact info',
 	'data-show-modal' => '#contact-dialog'
-])->append('svg', null, [
-	'height' => 50,
-	'width' => 50
-])->append('use', null, [
-	'xlink:href' => 'images/icons.svg#person'
-]);
+])->import(\shgysk8zer0\DOM\SVG::useIcon('person', $size));
 
 $footer('contact-dialog');
 return $footer;
-// require_once './components/contact-dialog.php';
