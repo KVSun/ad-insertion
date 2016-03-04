cedf043f077453d68ed08ad02c46031c6867594e
Chris Zuber <shgysk8zer0@gmail.com>
2016-03-03 17:55:15 -0800

    Update icon usage in footer

--- components/footer.php
+++ components/footer.php
@@ -1,28 +1,40 @@
 <?php
 namespace Components\Footer;
+use \shgysk8zer0\DOM as DOM;
+
 function loadFooter()
 {
 	$size = ['height' => 50, 'width' => 50];
 	$params = new \shgysk8zer0\Core\URLSearchParams();
+
 	$params->load = 'readme';
-	$footer = \shgysk8zer0\DOM\HTML::getInstance()->createElement('footer');
+	$footer = DOM\HTML::getInstance()->createElement('footer');
 	$footer->append('a', null, [
 		'title' => 'View README/documentation',
 		'href' => "?{$params}",
 		'role' => 'button'
-	])->import(\shgysk8zer0\DOM\SVG::useIcon('book', $size));
+	])->import(DOM\SVG::useIcon('info', $size));
 
 	$params->load = 'ad_insertion';
 	$contact = $footer('contact_dialog')[0];
 	$footer->append('button', null, [
 		'title' => 'Contact info',
 		'data-show-modal' => "#{$contact->id}"
-	])->import(\shgysk8zer0\DOM\SVG::useIcon('person', $size));
-
-	$footer->append('a', 'Ad Insertion', ['href' => "?{$params}"]);
+	])->import(DOM\SVG::useIcon('organization', $size));
 
 	$params->load = 'login';
-	$footer->append('a', 'Login', ['href' => "?{$params}"]);
+	$footer->append('a', null, [
+		'href' => "?{$params}",
+		'title' => 'login',
+		'role' => 'button'
+	])->import(DOM\SVG::useIcon('sign-in', $size));
+
+	$footer->append('a', null, [
+		'href' => "?{$params}",
+		'title' => 'Ad Insertion',
+		'role' => 'button'
+	])->import(DOM\SVG::useIcon('smiley', $size));
+
 	return $footer;
 }
 return loadFooter();
