b95c1bd8d2e92af93233e1908fef5bc57ba764bd
kvsun-editor <editor@kvsun.com>
2016-03-03 16:11:47 -0800

    Use URLSearchParams instead of URL class
    This simplifies things a little bit, and should also perform slightly
    better

--- components/footer.php
+++ components/footer.php
@@ -3,25 +3,24 @@ namespace Components\Footer;
 function loadFooter()
 {
 	$size = ['height' => 50, 'width' => 50];
-	$url = \shgysk8zer0\Core\URL::getInstance();
 	$params = new \shgysk8zer0\Core\URLSearchParams();
 	$params->load = 'readme';
-	$url->query = "$params";
 	$footer = \shgysk8zer0\DOM\HTML::getInstance()->createElement('footer');
 	$footer->append('a', null, [
 		'title' => 'View README/documentation',
-		'href' => $url,
+		'href' => "?{$params}",
 		'role' => 'button'
 	])->import(\shgysk8zer0\DOM\SVG::useIcon('book', $size));
 
 	$params->load = 'ad_insertion';
-	$url->query = "$params";
 	$contact = $footer('contact_dialog')[0];
 	$footer->append('button', null, [
 		'title' => 'Contact info',
 		'data-show-modal' => "#{$contact->id}"
 	])->import(\shgysk8zer0\DOM\SVG::useIcon('person', $size));
-	$footer->append('a', 'Ad Insertion', ['href' => $url]);
+
+	$footer->append('a', 'Ad Insertion', ['href' => "?{$params}"]);
+	$params->load = 'circulation';
 	return $footer;
 }
 return loadFooter();
