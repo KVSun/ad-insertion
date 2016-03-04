15993b65aa76b7b233c17a246f113547623db707
Chris Zuber <shgysk8zer0@gmail.com>
2016-03-03 18:06:06 -0800

    Get contact info from package.json

--- components/contact_dialog.php
+++ components/contact_dialog.php
@@ -1,34 +1,41 @@
 <?php
 namespace Components\Contact_Dialog;
+use \shgysk8zer0\DOM\SVG as SVG;
 function loadDialog()
 {
 	$size = ['height' => 60, 'width' => 60];
+	$package = json_decode(file_get_contents('package.json'));
 	$dialog = \shgysk8zer0\DOM\HTML::getInstance()->createElement('dialog');
 	$dialog->id = 'contact-dialog';
+
 	$dialog->append('button', null, [
 		'type' => 'button',
 		'data-close' => "#{$dialog->id}"
 	]);
 	$dialog->append('br');
+
 	$dialog->append('a', null, [
-		'href' => 'mailto:editor@kvsun.com',
+		'href' => "mailto:{$package->author->email}",
 		'title' => 'Email'
-	])->import(\shgysk8zer0\DOM\SVG::useIcon('mail-read', $size), true);
+	])->import(SVG::useIcon('mail-read', $size), true);
+
 	$dialog->append('a', null, [
 		'href' => 'https://gitter.im/KVSun/ad-insertion',
 		'target' => '_blank',
 		'title' => 'Chat on Gitter'
-	])->import(\shgysk8zer0\DOM\SVG::useIcon('comment', $size), true);
+	])->import(SVG::useIcon('comment', $size), true);
+
 	$dialog->append('a', null, [
-		'href' => 'https://github.com/KVSun/ad-insertion/',
+		'href' => str_replace('.git', null ,$package->repository->url),
 		'target' => '_blank',
 		'title' => 'GitHub'
-	])->import(\shgysk8zer0\DOM\SVG::useIcon('mark-github', $size), true);
+	])->import(SVG::useIcon('mark-github', $size), true);
+
 	$dialog->append('a', null, [
-		'href' => 'https://github.com/KVSun/ad-insertion/issues/new/',
+		'href' => $package->bugs->url,
 		'target' => '_blank',
 		'title' => 'Open Issue'
-	])->import(\shgysk8zer0\DOM\SVG::useIcon('issue-opened', $size), true);
+	])->import(SVG::useIcon('issue-opened', $size), true);
 	return $dialog;
 }
 return loadDialog();
