030a5c9f8d4cde22cb5d80a6cfb72300decc53a9
Chris Zuber <shgysk8zer0@gmail.com>
2016-03-12 13:47:13 -0800

    Improve building response in index.php
    - All setup occurs in `init`
    - Check for `application/json` Accept header first
    - Send Console header before returning response
    - `require_once` the autoloader script in case not using Apache

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 namespace Index;
+
 use \shgysk8zer0\Core as Core;
 use \shgysk8zer0\Core_API as API;
 use \shgysk8zer0\DOM as DOM;
-
-Core\Console::getInstance()->asErrorHandler()->asExceptionHandler();
-DOM\HTMLElement::$import_path = realpath(getenv('COMPONENTS_DIR'));
 
 function build_head(\DOMElement &$head)
 {
@@ -21,19 +19,25 @@ function build_body(\DOMElement &$body)
 
 function init()
 {
+	require_once realpath(getenv('AUTOLOAD_SCRIPT'));
+	Core\Console::getInstance()->asErrorHandler()->asExceptionHandler();
+	DOM\HTMLElement::$import_path = realpath(getenv('COMPONENTS_DIR'));
+
 	$headers = Core\Headers::getInstance();
 	$dom     = DOM\HTML::getInstance();
 
-	if (in_array('text/html', explode(',', $headers->accept))) {
+	if ($headers->accept === 'application/json' and !empty($_REQUEST)) {
+		require_once './components/handlers/request.php';
+		$resp = \Components\Handlers\Request\handle($_REQUEST);
+		Core\Console::getInstance()->sendLogHeader();
+		return $resp;
+	} elseif ($headers->accept = '*/*' or in_array('text/html', explode(',', $headers->accept))) {
 		build_head($dom->head);
 		build_body($dom->body);
+		Core\Console::getInstance()->sendLogHeader();
 		return $dom;
-	} elseif ($headers->accept === 'application/json' and !empty($_REQUEST)) {
-		require_once './components/handlers/request.php';
-		return \Components\Handlers\Request\handle($_REQUEST);
 	} else {
 		http_response_code(API\Abstracts\HTTPStatusCodes::BAD_REQUEST);
 	}
 }
-Core\Console::getInstance()->sendLogHeader();
 exit(init());
