a405f49c8a89dab59df7b89a40131ed8626cf8a5
kvsun-editor <editor@kvsun.com>
2016-03-14 08:08:24 -0700

    Update SVGSprites
    - Add namespace
    - Allow Autoloader to set and use environment vars
    - Use aliases of Namespaces and classes
    - Exit with status of 403 if not CLI

--- svgsprites.php
+++ svgsprites.php
@@ -1,9 +1,14 @@
 <?php
-putenv('MIN_PHP_VERSION=5.5');
-putenv('AUTOLOAD_DIR=classes');
-putenv('AUTOLOAD_FUNC=spl_autoload');
-putenv('AUTOLOAD_EXTS=.php');
+namespace SVGSprites;
+
+use \shgysk8zer0\DOM as DOM;
+use \shgysk8zer0\Core_API\Abstracts\HTTPStatusCodes as HTTPStatus;
+
 require_once './autoloader.php';
-$icons = json_decode(file_get_contents('images/icons.json'), true);
-$sprites = new \shgysk8zer0\DOM\SVGSprite($icons);
-$sprites->save('images/icons.svg');
+if (in_array(PHP_SAPI, ['cli'])) {
+	$icons = json_decode(file_get_contents('images/icons.json'), true);
+	$sprites = new DOM\SVGSprite($icons);
+	$sprites->save('images/icons.svg');
+} else {
+	http_response_code(HTTPStatus::FORBIDDEN);
+}
