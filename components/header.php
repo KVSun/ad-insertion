d973a41afc422b2e5bb2aeb81e458d0e0d93b27d
kvsun-editor <editor@kvsun.com>
2016-04-01 11:22:47 -0700

    Import sun.svg as HTML instead of using <img>

--- components/header.php
+++ components/header.php
@@ -5,12 +5,7 @@ function loadHeader()
 	$header = \shgysk8zer0\DOM\HTML::getInstance()->createElement('header');
 	$header->append('h1', 'KV Sun Ad Insertion', [
 		'class' => 'center'
-	])->append('img', null, [
-		'alt' => 'KV Sun Logo',
-		'src' => 'sun.svg',
-		'width' => 266,
-		'height' => 90
-	]);
+	])->importHTML(file_get_contents('sun.svg'));
 	return $header;
 }
 return loadHeader();
